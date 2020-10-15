21c8f8a2d252489a494596815f885e41964cc888
rajat gupta <rajat_raj@excellencetechnologies.in>
2020-10-15 12:49:54 +0530

    make it better

--- keystone.js
+++ keystone.js
@@ -3,9 +3,9 @@
 // require('dotenv').config();
 
 
-// if (process.env.NODE_ENV !== 'production') { 
-// 	require('dotenv').config() 
-// }
+if (process.env.NODE_ENV !== 'production') { 
+	require('dotenv').config() 
+}
  
 
 // Require keystone
