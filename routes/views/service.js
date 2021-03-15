699e7321321e9e395e5f85b891cd6807af3df064
Rahul <rahul.excel2011@gmail.com>
2021-03-15 17:57:02 +0530

    updated for food plan

--- routes/views/service.js
+++ routes/views/service.js
@@ -919,7 +919,7 @@ Your menu for the week is:<br/>`
 		}
 	},
 	previousFoodPlanUpdate: async () => {
-		let allUsers = await users.model.find({blockFoodPlan: false}).lean();
+		let allUsers = await users.model.find({'blockFoodPlan': false}).lean();
 		let upcomingSunday = new Date();
 		upcomingSunday = upcomingSunday.setDate(
 			upcomingSunday.getDate() + ((0 - 1 - upcomingSunday.getDay() + 7) % 7) + 1
